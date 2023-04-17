26f24a77ec6ee01eee1223d1c982f62f53eaf5c9
Tim Hunter <thunte27@uwo.ca>
2023-04-17 00:33:21 -0400

    remove Node class

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Book"},{"p":"<Unnamed>","l":"BookList"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Node"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Book"},{"p":"<Unnamed>","l":"BookList"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
